6d54e9192d8c4ef55700aef127feedb6026eb21d
Damiisoks <damieteisokariari@gmail.com>
2026-09-10 19:04:27 +0100

    initial commit

---
+++ intro.js
@@ -0,0 +1,3 @@
+console.log("Hello, my name is Damiete soibiton Isokariari. I am 19 years old and" +
+" I dont really have a favorite programming language but i am trying to learn python. My unique number is BD26090363");
+// name, age, and favorite programming language
